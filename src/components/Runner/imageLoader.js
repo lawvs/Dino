065da087495f3be348dc073c6245b84231f5f96a
lawvs <lawvs@users.noreply.github.com>
2018-07-26 02:07:19 +0800

    feat: add imageLoader

---
+++ src/components/Runner/imageLoader.js
@@ -0,0 +1,33 @@
+import cloudImg from './images/cloud.png'
+
+/**
+ * URL to load
+ * @type {string[]} img url array
+ */
+const imageArray = [
+    cloudImg,
+]
+
+/**
+ * @type {Map<string, HTMLImageElement>}
+ */
+const imageMap = new Map()
+
+const promiseArray = imageArray.map(imgUrl => {
+    const promise = new Promise((resolve, reject) => {
+        const img = new Image()
+        img.onerror = reject
+        img.onload = () => {
+            imageMap.set(imgUrl, img)
+            resolve()
+        }
+        img.src = imgUrl
+    })
+    return promise
+})
+
+export function loadImages() {
+    return Promise.all(promiseArray)
+}
+
+export default imageMap
