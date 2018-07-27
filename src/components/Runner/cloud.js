7c2c90d349b8de4ef2ee7e89b41511299151b89a
lawvs <lawvs@users.noreply.github.com>
2018-07-27 19:47:13 +0800

    refactor: cloud

--- src/components/Runner/cloud.js
+++ src/components/Runner/cloud.js
@@ -1,21 +1,11 @@
 // @ts-check
 /// <reference path='./index.d.ts'/>
-import { castArray, random, sample } from 'lodash'
+import { isNumber, random } from 'lodash'
 
-import imageMap from './imageLoader'
+import Sprite from './sprite'
 import defaultCloudImg from './images/cloud.png'
 
-class Cloud {
-    /** @type {!HTMLCanvasElement} */
-    canvas
-    /** @type {!CanvasRenderingContext2D} */
-    canvasCtx
-    /** @type {number} */
-    xPos
-    /** @type {number} */
-    yPos
-    /** @type {HTMLImageElement | HTMLCanvasElement | HTMLVideoElement | ImageBitmap} */
-    img
+class Cloud extends Sprite {
     /** @type {boolean} */
     remove = false
 
@@ -25,9 +15,7 @@ class Cloud {
      */
     config = {
         /** @type {Array | string} */
-        IMG_SRC: [defaultCloudImg],
-        X_POS: null, // initial x position
-        Y_POS: null, // initial y position
+        IMG_SRC: defaultCloudImg,
         MAX_SKY_LEVEL: null,
         MIN_SKY_LEVEL: null,
     };
@@ -37,24 +25,14 @@ class Cloud {
      * @param {object} [options={}]
      * @constructs Cloud
      */
-    constructor(canvas, options={}) {
-        if (!canvas) {
-            // this.remove = true
-            throw new Error('the parameter canvas is required!')
-        }
-        this.canvas = canvas
-        this.canvasCtx = this.canvas.getContext('2d')
+    constructor(canvas, options = {}) {
+        super(canvas)
         this.config = {
             ...this.config,
             ...options,
         }
-        this.config.IMG_SRC = castArray(this.config.IMG_SRC)
-        this.img = imageMap.get(sample(this.config.IMG_SRC))
-        if (!this.img) {
-            throw new Error(`load cloud image fail! IMG_SRC: ${this.config.IMG_SRC}`)
-        }
-        this.config.MAX_SKY_LEVEL = this.config.MAX_SKY_LEVEL || this.img.height
-        this.config.MIN_SKY_LEVEL = this.config.MIN_SKY_LEVEL || this.canvas.height / 2 - this.img.height
+        isNumber(this.config.MAX_SKY_LEVEL) || (this.config.MAX_SKY_LEVEL = this.img.height)
+        isNumber(this.config.MIN_SKY_LEVEL) || (this.config.MIN_SKY_LEVEL = this.canvas.height / 2 - this.img.height)
 
         this.xPos = this.config.X_POS || this.canvas.width
         this.yPos = this.config.Y_POS || random(
