8d4c8952e5c3d75dcd3fbb9a838ab0fe2b097ad8
lawvs <lawvs@users.noreply.github.com>
2018-08-21 19:36:16 +0800

    feat: add i18n

--- src/components/MessageCard/index.js
+++ src/components/MessageCard/index.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
-
+import i18n from '../utils/i18next' // eslint-disable-line
+import { I18n } from 'react-i18next'
 import './styles.css'
 
 class MessageCard extends Component {
     render() {
         return (
-            <div className="messagecard">
-                <h1 className="title">未连接到互联网</h1>
-            </div>
+            <I18n>
+                {(t, { i18n }) => (
+                    <div className="messagecard">
+                        <h1 className="title">{t('No internet connection')}</h1>
+                    </div>
+                )}
+            </I18n>
         )
     }
 }
---
+++ src/components/utils/i18next.js
@@ -0,0 +1,37 @@
+import { zipObject } from 'lodash'
+import { createInstance } from 'i18next'
+import { reactI18nextModule } from 'react-i18next'
+
+import en_US from '../../i18n/en-US.json'
+import ja_JP from '../../i18n/ja-JP.json'
+import zh_CN from '../../i18n/zh-CN.json'
+
+const LOCALES = ['zh-CN', 'ja-JP', 'en-US']
+const resources = [zh_CN, ja_JP, en_US].map(t => {
+    return { translation: t }
+})
+const mainRes = zipObject(LOCALES, resources)
+
+const standardizeLocale = language => {
+    if (LOCALES.includes(language)) return language
+    switch (language.substr(0, 2).toLowerCase()) {
+    case 'zh':
+        return 'zh-CN'
+    case 'ja':
+        return 'ja-JP'
+    default:
+        return 'en-US'
+    }
+}
+
+const language = standardizeLocale(navigator.language)
+
+const i18next = createInstance()
+i18next.use(reactI18nextModule).init({
+    lng: language,
+    debug: true,
+    resources: mainRes,
+})
+
+export const t = i18next.getFixedT()
+export default i18next
