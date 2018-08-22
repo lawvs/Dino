aa4266a05d52e9a1ce16e13fe256af3673fa243e
lawvs <lawvs@users.noreply.github.com>
2018-08-22 17:13:30 +0800

    refactor: use function in MessageCard

--- src/components/MessageCard/index.js
+++ src/components/MessageCard/index.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { I18n } from 'react-i18next'
 
 import i18n from '../utils/i18next' // eslint-disable-line
 import './styles.css'
 
-class MessageCard extends Component {
-    errorCode = 'ERR_INTERNET_DISCONNECTED'
-    suggests = [
+const MessageCard = () => {
+    const errorCode = 'ERR_INTERNET_DISCONNECTED'
+    const suggests = [
         'Checking the network cables, modem and router',
         'Reconnecting to Wi-Fi',
     ]
 
-    render() {
-        return (
-            <I18n>
-                {t => (
-                    <div className="messagecard">
-                        <h1 className="title">{t('No internet connection')}</h1>
-                        <p>{t('Try')}</p>
-                        <ul>
-                            {this.suggests.map(s => (
-                                <li key={s}>{t(s)}</li>
-                            ))}
-                        </ul>
-                        <div className="error-code">{this.errorCode}</div>
-                    </div>
-                )}
-            </I18n>
-        )
-    }
+    return (
+        <I18n>
+            {t => (
+                <div className="messagecard">
+                    <h1 className="title">{t('No internet connection')}</h1>
+                    <p>{t('Try')}</p>
+                    <ul>
+                        {suggests.map(s => (
+                            <li key={s}>{t(s)}</li>
+                        ))}
+                    </ul>
+                    <div className="error-code">{errorCode}</div>
+                </div>
+            )}
+        </I18n>
+    )
 }
 
 export default MessageCard
