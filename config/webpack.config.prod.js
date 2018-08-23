032edb5521be46886fba1a6d7c7552e9f2ed5c7f
lawvs <lawvs@users.noreply.github.com>
2018-08-23 18:01:11 +0800

    chore: modify performance limit

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -312,6 +312,10 @@ module.exports = {
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
   ],
+  performance: {
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
   node: {
